8d8a00ae0746941bac87ccf6244f3448a45e71da
vaithy2410 <eshwar2410@gmail.com>
2023-10-31 13:00:00 +0530

    sample

---
+++ src/org/array/Sum.java
@@ -0,0 +1,12 @@
+package org.array;
+
+public class Sum {
+	  public static void main(String[] args) {  
+	        int [] arr = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};  
+	        int sum = 0;  
+	        for (int i = 0; i < arr.length; i++) {  
+	           sum = sum + arr[i];  
+	        }  
+	        System.out.println("Sum of array: " + sum);  
+	    }  
+	}
